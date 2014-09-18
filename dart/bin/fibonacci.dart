3f2f652c00dc07e2f2aa8038c09b08bd6137a978
Romain, Francois <romfranc@cisco.com>
2014-09-18 12:14:59 +0200

    [Samples] Adding the Fibonacci example

---
+++ dart/bin/fibonacci.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+debug(String msg) {
+    stderr.writeln(msg);
+}
+
+
+//int fibonacci(int number) {
+//  if (number == 0) {
+//    return 0;
+//  }
+//  else if (number == 1) {
+//    return 1;
+//  }
+//  else return fibonacci(number - 1) + fibonacci(number - 2);
+//}
+
+// O(n)
+int fibonacci(int number) {
+  List<int> result = [0, 1];
+
+  if(number < 2) {
+      return result[number];
+  }
+
+  int  fibNMinusOne = 1;
+  int  fibNMinusTwo = 0;
+  int  fibN = 0;
+
+  for(int i = 2; i <= number; i++) {
+    fibN = fibNMinusOne + fibNMinusTwo;
+
+    fibNMinusTwo = fibNMinusOne;
+    fibNMinusOne = fibN;
+  }
+
+  return fibN;
+}
+
+// O(log n) can be achieved using matrix
+
+void main() {
+  int input = 15;
+
+  debug('Result: ${fibonacci(input)}');
+}
